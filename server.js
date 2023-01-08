1f2fad9f6d16da0dfbfd382b29575337388e7fc5
shivani awasthi <shvaniawsthi001@gmail.com.com>
2023-01-08 16:26:07 +0530

    user

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use(cors())
+// server.use(cors())
 server.use("/api", router);
 server.listen(PORT, () =>
   console.log(`JSON Server is running on port ${PORT}`)
